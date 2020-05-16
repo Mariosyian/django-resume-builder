1e7c5b50e9765f4096f133fa4266be2e936defb0
Marios Yiannakou <marios.yiannakou@hotmail.com>
2020-05-16 14:16:05 +0100

    Refactor code and documentation.

--- apps/user_resume/forms.py
+++ apps/user_resume/forms.py
@@ -2,12 +2,13 @@ from django import forms
 
 from .models import Resume
 
-
 class CreateResumeForm(forms.ModelForm):
     """
-    A form for creating a new user resume. Note that 'user' is not
-    included: it is always set to the requesting user. As well as
-    the number of items is not included: defaults to 0.
+    A form for creating a new user resume. Note that:
+    'user' is always set to the requesting user.
+    'num_items': defaults to 0.
+    'date_created': gets the current time of creation.
+    'date_modified': gets the current time of update.
     """
     class Meta:
         model = Resume
